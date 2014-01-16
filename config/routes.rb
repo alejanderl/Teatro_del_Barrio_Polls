090924c4bd8559a23951b05cd99f6182118ea2e5
José Manuel <Manu@MacBook-Pro-de-Jose-Manuel.local>
2014-01-16 01:13:10 +0000

    Configured DB fro pg. Added landing page and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TeatroDelBarrio::Application.routes.draw do
+  root :to => "home#index"
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +55,6 @@ TeatroDelBarrio::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  
 end
