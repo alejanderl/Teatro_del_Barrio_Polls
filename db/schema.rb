da4cb326d7576d072ca123496afba1958c7b67b1
alejanderl <janderl@gmail.com>
2014-01-29 00:03:27 +0100

    added poll model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140116005908) do
+ActiveRecord::Schema.define(version: 20140128220545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "polls", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.text     "answers"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  create_table "questions", force: true do |t|
+    t.integer  "poll_id"
+    t.string   "matter"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
---
+++ spec/features/visit_home_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+
+describe "Set" do
+  describe "GET /password_resets" do
+    it "works! (now write some real specs)", :focus do
+    	debugger
+    	visit "/es"
+    	
+    	
+    	sleep 1
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+
+    end
+  end
+end
