721d10c580cfcacd38803f06dc0d6575db50bb92
alejanderl <janderl@gmail.com>
2014-01-31 16:58:23 +0100

    mend

--- spec/features/visit_home_spec.rb
+++ spec/features/visit_home_spec.rb
@@ -5,13 +5,14 @@ describe "Set" do
 
     it "works! (now write some real specs)", :focus do
     	
-    	visit "/es"
-    	debugger
-    	visit root_path
     	
-    	sleep 1
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    	
+    	visit root_path
+    	visit "/en"
+		assert (I18n.locale == :en)
 
-
+    	visit "/es"
+    	assert (I18n.locale == :es)
+    	
   end
 end
