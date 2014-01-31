843b0d5a8b560619daac7154d9626549d33145af
José Manuel <Manu@MacBook-Pro-de-Jose-Manuel.local>
2014-01-31 07:53:20 +0000

    solo syncro

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :memberships
   has_many :roles, :through => :memberships
+
+
+  def active_for_authentication?
+    super && self.enabled
+ 
+  end
 end
