42a2144ef7aec03a757015fa285af56f72c6d831
alejanderl <janderl@gmail.com>
2014-02-05 11:03:11 +0100

    mend

--- spec/features/polls_spec.rb
+++ spec/features/polls_spec.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 describe "Polls testing"   do
   
   before :each do
@@ -18,11 +19,27 @@ describe "Polls testing"   do
 
   	click_link "Nuevo"
 
-  	
+    fill_in "poll_title", :with => "Título de la votación"
+    fill_in "poll_description", :with => "Título de la votación"
+    fill_in "poll_start_date", :with => Time.now
+    fill_in "poll_end_date", :with => Time.now + 1.month
+    4.times do 
+      click_link "add_question"
+    end
+
+    within "#questions" do
+      
+      all(:xpath ,".//input[@type='text']").each do |element|
+        debugger
+        fill_in element, :with => "prueba"
+      end
+    end
+    debugger
+
 
   	sleep 1
 
-
+    sleep 1
 
   end
 
