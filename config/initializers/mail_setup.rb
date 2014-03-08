1b6dba050acb50b844bad188723ea2caa82512fa
alejanderl <janderl@gmail.com>
2014-03-08 18:33:53 +0100

    Added mandrill setup and local variables. Pushed some ENV to Heroku

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -5,8 +5,11 @@ ActionMailer::Base.smtp_settings = {
     :password             => ENV["MANDRILL_API_KEY"],
     :enable_starttls_auto => true, # detects and uses STARTTLS
     :authentication => 'plain', # Mandrill supports 'plain' or 'login'
-    :domain => 'beta.propiedadabierta.com' # your domain to identify your server when connecting
+    :domain => 'teatrodelbarrio.herokuapp.com' # your domain to identify your server when connecting
  }
 
-ActionMailer::Base.default_url_options[:host] = "beta.propiedadabierta.com"
+ActionMailer::Base.default_url_options[:host] = "teatrodelbarrio.herokuapp.com"
+
+
+
 
