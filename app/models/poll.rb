475bab587b8c7c9419e6c792577799fd725dcbb2
alejanderl <janderl@gmail.com>
2014-03-09 01:08:26 +0100

    mend

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -12,6 +12,8 @@ class Poll < ActiveRecord::Base
 
 	validates :user_id, :start_date, :end_date, :title, :presence => :true
 
+	validate :is_editable?
+
 
 	# Each poll has at least one question. Question matter must be present.
 	#before_validation {|_this| _this.questions.first || _this.questions.build  }
@@ -22,6 +24,12 @@ class Poll < ActiveRecord::Base
 		self.start_date < now && self.end_date > now
 	end
 
+	def is_editable?
+		#not editable if there's already answers
+		self.questions.each do |question|
+			return false if question.answers.count > 0
+		end
+	end
 
 
 
