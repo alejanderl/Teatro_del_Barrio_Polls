53c0921081d1b7960697513835ac5127f03cb235
alejanderl <janderl@gmail.com>
2014-03-10 10:29:50 +0100

    Some tests added

--- spec/features/polls_spec.rb
+++ spec/features/polls_spec.rb
@@ -56,20 +56,46 @@ describe "Polls testing"   do
       
     end
 
-  it "Members can vote" ,:focus  , :js do
+  it "Members can vote"  do
     create_poll
     user_login "member@example.com", "member123"
     
     visit poll_path(Poll.last.id, :locale => :es)
     #vote yes
     first(".btn-success").click
-
+    page.should have_content "Has votado si"
     within ".vote-results" do
       within ".list-group-item-success" do
         page.should have_content "1"
       end
 
     end
+    click_link "Cambiar mi voto"
+
+    page.should_not have_content "Has votado si"
+    within ".vote-results" do
+      within ".list-group-item-success" do
+        page.should have_content "0"
+      end
+    end
+    first(".btn-warning").click
+
+    page.should have_content "Has votado no"
+    within ".vote-results" do
+      within ".list-group-item-warning" do
+        page.should have_content "1"
+      end
+    end
+  end
+
+  it "users can not vote experired polls"   do
+
+    create_poll :start_date => (Time.now - 7.days), :end_date => (Time.now - 1.day)
+    user_login "member@example.com", "member123"
+    
+    visit poll_path(Poll.last.id, :locale => :es)
+    page.should have_content("Cerrada")
+    page.should have_content("No has votado")
 
 
 
